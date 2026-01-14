b69a88d2d14bd31d3c2af3d11fb04537e0e1311c
kathir111 <123354808+kathir111@users.noreply.github.com>
2026-01-14 11:52:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 
 // public folder for users profile
 app.use('/profileImgs', express.static(path.join(__dirname, 'public/profileImgs')));
