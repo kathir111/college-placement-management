e6a6980f26993041c2dc27427ad3b003c6a2c5c6
kathir111 <123354808+kathir111@users.noreply.github.com>
2026-01-15 19:30:51 +0530

    ccc

--- backend/controllers/SuperUser/login.controller.js
+++ backend/controllers/SuperUser/login.controller.js
@@ -9,6 +9,10 @@ const Login = async (req, res) => {
     const user = await StudentUser.findOne({ email });
     if (!user)
       return res.status(400).json({ msg: "User Doesn't Exist!" });
+        console.log("INPUT EMAIL:", email);
+    console.log("DB EMAIL:", user.email);
+    console.log("DB ROLE:", user.role);
+    console.log("DB HASH:", user.password);
 
     const isMatch = await bcrypt.compare(password, user.password);
     
